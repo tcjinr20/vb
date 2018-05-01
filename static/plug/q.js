054f7db740702ccba9d1912c16a8a8895564318f
tcj <tcj@163.com>
2018-05-01 22:17:36 +0800

    layer

---
+++ static/plug/q.js
@@ -0,0 +1,1 @@
+var pack=[{"attr":"retr","child":"99","id":"11"},{"attr":"retr","child":"99","id":"11"}]
---
+++ static/q.js
@@ -0,0 +1,1 @@
+var pack=[{"LAY_TABLE_INDEX":0,"attr":"retr","child":"99","id":"11"},{"LAY_TABLE_INDEX":1,"attr":"retr","child":"99","id":"11"}]
